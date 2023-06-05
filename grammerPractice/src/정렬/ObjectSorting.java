b487b46bdf532df2c2c5e69f7dfc7c20a4939bd5
heenamkang <abc_always@naver.com>
2023-06-05 10:28:23 +0900

    comparingInt sorting

--- grammerPractice/src/정렬/ObjectSorting.java
+++ grammerPractice/src/정렬/ObjectSorting.java
@@ -15,6 +15,7 @@ public class ObjectSorting {
 
         List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
 
+
         //1.comparator 정렬
         Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
             @Override
@@ -25,9 +26,9 @@ public class ObjectSorting {
 
         /*
         1-*.정리
-        Collections.sort(컬렉션, new Comparator<원소자료형>() {
+        Collections.sort(컬렉션, new Comparator<비교원소 자료형>() {
             @Override
-            public int compare(원소자료형 o1, 원소자료형 o2) {
+            public int compare(비교원소 자료형 o1, 비교원소 자료형 o2) {
                 return o1.비교대상getter - o2.비교대상getter;
                 return o1.getValue() - o2.getValue(); //오름차순
             }
@@ -42,10 +43,10 @@ public class ObjectSorting {
 
          */
 
-        //2-1.람다 정렬 - 비교대상 Comparable 구현 ex : Integer
+        //2-1.람다 정렬 - 비교대상 Comparable구현 ex : Integer
         Collections.sort(list , (o1, o2) -> o1.getValue().compareTo(o2.getValue()));
 
-        //2-2.람다 정렬 - 비교대상 Comparable 미구현 ex : int
+        //2-2.람다 정렬 - 비교대상 Comparable미구현 ex : int
         Collections.sort(list, (o1, o2) -> o2.getValue() - o1.getValue()); // 내림차순 소팅
         System.out.println(list);
 
@@ -56,5 +57,12 @@ public class ObjectSorting {
         문자열.compareTo(문자열) -> 처음문자부터 비교 - 포함 -> 길이차 return
                                                다른지점O -> 다른지점 문자 아스키코드 차 return
          */
+
+        //3-1.comparingInt정렬 - 오름차순
+        Collections.sort(list , Comparator.comparingInt(o->o.getValue()));
+
+        //3-2.comparingInt 정렬 - 내림차순
+        Collections.sort(list , Comparator.comparingInt(o->o.getValue()));
+        Collections.reverse(list);
     }
 }
